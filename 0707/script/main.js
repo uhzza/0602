bbdfad2dc1865e03d1af46ee1fdcaeb9916c06f2
uhzza <whdgns955@naver.com>
2022-07-11 08:15:32 +0900

    0711

---
+++ 0707/script/main.js
@@ -0,0 +1,6 @@
+const init=()=>{
+    $('header').load('/0707/fragment/header.html');
+    $('nav').load('/0707/fragment/nav.html');
+    $('aside').load('/0707/fragment/aside.html');
+    $('footer').load('/0707/fragment/footer.html');
+  }
---
+++ board/script/main.js
@@ -0,0 +1,6 @@
+const init=()=>{
+    $('header').load('/board/fragment/header.html');
+    $('nav').load('/board/fragment/nav.html');
+    $('aside').load('/board/fragment/aside.html');
+    $('footer').load('/board/fragment/footer.html');
+}
